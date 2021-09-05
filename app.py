0da229be79c4d15cf4e46100d35761ec45225871
vnurhaqiqi <viqinurhaqiqi@gmail.com>
2021-09-05 19:58:22 +0700

    setting up docker

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask_restful import Api, Resource
 from .helpers.response import APIResponse
 
+import os
+
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
@@ -10,4 +12,5 @@ CORS(app)
 from .routes import route
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
